bb1411254738e2daafc324fc062461f570bdccf8
ya903040 <asirov560@gmail.com>
2024-12-24 04:04:46 +0100

    update

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import qrcode
-import datetime
+from PIL import Image
 
 
-data = "https://drive.google.com/drive/folders/1wv3uN03ZnRQq1MqzfftlQrd6Ys2-nujb?usp=sharing"
+data = "https://docs.google.com/document/d/1bEcSw-jE6D5NfAUqn4wvyyGECN2tTMRjTqb6kPQqk38/edit?usp=sharing"
 
-current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-file_name = f"qr_code/qrcode_{current_time}.png"
 
-def create_qr_code(data, file_name):
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(data)
-    qr.make(fit=True)
-    img = qr.make_image(fill='black', back_color='white')
-    img.save(file_name)
-    print(f"QR-код успешно создан и сохранен как {file_name}")
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
 
-create_qr_code(data, file_name)
+
+qr.add_data(data)
+qr.make(fit=True)
+
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+
+img.save("qrcode.png")
+
+
+img.show()
